455bc3c60f21be56defa2890b44481f967f415f9
KentPhan <u0734504@ad.utah.edu>
2019-01-15 00:16:19 -0700

    Added basic enemies and canvas manager

--- BricolageProject/Assets/Scripts/BasicEnemyScript.cs
+++ BricolageProject/Assets/Scripts/BasicEnemyScript.cs
@@ -4,15 +4,37 @@ namespace Assets
 {
     public class BasicEnemyScript : MonoBehaviour
     {
+        [SerializeField]
+        private float m_Speed;
+
+
+        private Rigidbody2D m_RigidBody;
+
         // Start is called before the first frame update
         void Start()
         {
 
+            m_RigidBody = GetComponent<Rigidbody2D>();
         }
 
         // Update is called once per frame
         void Update()
         {
+            GameObject l_Target = GameManager.Instance.GetPlayer();
+            Vector2 l_Direction = (l_Target.GetComponent<Rigidbody2D>().position - m_RigidBody.position).normalized;
+            m_RigidBody.MovePosition(m_RigidBody.position + l_Direction * m_Speed * Time.deltaTime);
+        }
+
+        public void OnTriggerEnter2D(Collider2D i_collider)
+        {
+            if (i_collider.gameObject.CompareTag("Weapon"))
+                Destroy(this.gameObject);
+            else if (i_collider.gameObject.CompareTag("Player"))
+            {
+                Destroy(i_collider.gameObject);
+                GameManager.Instance.TriggerGameOver();
+            }
+
 
         }
     }
--- BricolageProject/Assets/Scripts/GameManager.cs
+++ BricolageProject/Assets/Scripts/GameManager.cs
@@ -4,18 +4,32 @@ namespace Assets
 {
     public class GameManager : MonoBehaviour
     {
-
+        // Prefabs
         [SerializeField] private PlayerScript PlayerPrefab;
         [SerializeField] private GameObject PlayerSpawn;
         [SerializeField] private BasicEnemyScript EnemyPrefab;
         [SerializeField] private WeaponPieceScript WeaponPrefab;
 
 
+        // Boundaries
         [SerializeField] private GameObject WidthBoundary;
         [SerializeField] private GameObject HeightBoundary;
 
+        // Enemy Stuff
+        [SerializeField] private float InitialEnemySpawnRate = 5.0f;
+        [Range(0.0f, 1.0f)]
+        [SerializeField] private float SpawnRateSpeedUp = 0.9f;
+
+        [SerializeField] private float SpawnRateStageTimer = 10.0f;
+
+        private float m_CurrentEnemySpawnTimer;
+        private float m_CurrentEnemySpawnRate;
+        private float m_CurrentStageTimer;
+
+
 
         public static GameManager Instance;
+
         private PlayerScript m_CurrentPlayer;
 
         private Vector2 m_Boundary;
@@ -40,22 +54,56 @@ namespace Assets
 
             // Boundaries
             m_Boundary = new Vector2(Mathf.Abs(WidthBoundary.transform.position.x), Mathf.Abs((HeightBoundary.transform.position.y)));
+
+            // Enemy Stuff
+            m_CurrentEnemySpawnTimer = InitialEnemySpawnRate;
+            m_CurrentEnemySpawnRate = InitialEnemySpawnRate;
+            m_CurrentStageTimer = SpawnRateStageTimer;
         }
 
         // Update is called once per frame
         void Update()
         {
+            float m_deltaTime = Time.deltaTime;
 
+            // Enemy Spawner
+            if (m_CurrentEnemySpawnTimer <= 0)
+            {
+                Vector2 m_SpawnPosition = new Vector2(0, 0);
+                BasicEnemyScript m_newEnemy = Instantiate(EnemyPrefab, m_SpawnPosition, Quaternion.identity);
+                m_newEnemy.transform.SetParent(this.transform);
+
+                m_CurrentEnemySpawnTimer = m_CurrentEnemySpawnRate;
+            }
+
+
+            // Stage Adjuster
+            if (m_CurrentStageTimer <= 0)
+            {
+                m_CurrentEnemySpawnRate *= SpawnRateSpeedUp;
+
+                m_CurrentStageTimer = SpawnRateStageTimer;
+            }
+
+            m_CurrentEnemySpawnTimer -= m_deltaTime;
+            m_CurrentStageTimer -= m_deltaTime;
         }
 
         public GameObject GetPlayer()
         {
-            return PlayerPrefab.gameObject;
+            return m_CurrentPlayer.gameObject;
         }
 
         public Vector2 GetBoundaries()
         {
             return m_Boundary;
+        }
+
+        public void TriggerGameOver()
+        {
+            m_CurrentEnemySpawnTimer = InitialEnemySpawnRate;
+            m_CurrentEnemySpawnRate = InitialEnemySpawnRate;
+            m_CurrentStageTimer = SpawnRateStageTimer;
         }
     }
 }
