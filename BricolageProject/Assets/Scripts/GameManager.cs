7eff95d84847dc333ba7806c39cedf1c13b0ee49
KentPhan <u0734504@ad.utah.edu>
2019-01-14 23:09:04 -0700

    Initial Stuff

---
+++ BricolageProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        [SerializeField] private PlayerScript PlayerPrefab;
+        [SerializeField] private GameObject PlayerSpawn;
+        [SerializeField] private BasicEnemyScript EnemyPrefab;
+        [SerializeField] private WeaponPieceScript WeaponPrefab;
+
+        public static GameManager Instance;
+        private PlayerScript m_CurrentPlayer;
+
+        public void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+
+            else if (Instance != this)
+                Destroy(this);
+
+            DontDestroyOnLoad(this);
+        }
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // Spawn Player Object
+            m_CurrentPlayer = Instantiate(PlayerPrefab, PlayerSpawn.transform.position, Quaternion.identity);
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public GameObject GetPlayer()
+        {
+            return PlayerPrefab.gameObject;
+        }
+    }
+}
---
+++ BricolageProject/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class PlayerScript : MonoBehaviour
+    {
+
+        [SerializeField] private float m_Speed = 100;
+
+        private Rigidbody2D m_RigidBody;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            m_RigidBody = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector2 l_velocity = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * m_Speed;
+
+            m_RigidBody.position += (l_velocity * Time.deltaTime);
+        }
+    }
+}
